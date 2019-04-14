6dfc911850ffd2d5194c1b79a0a26f9474dbda58
Kenzie Togami <ket1999@gmail.com>
2019-04-14 13:19:28 -0700

    Add artifactory publishing

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,4 +13,5 @@ dependencies {
     implementation(gradleKotlinDsl())
     implementation("gradle.plugin.net.minecrell:licenser:0.4.1")
     implementation("net.ltgt.apt-idea:net.ltgt.apt-idea.gradle.plugin:0.21")
+    implementation("org.jfrog.buildinfo:build-info-extractor-gradle:4.9.5")
 }
--- buildSrc/src/main/kotlin/common.kt
+++ buildSrc/src/main/kotlin/common.kt
@@ -1,25 +1,42 @@
 import net.minecrell.gradle.licenser.LicenseExtension
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
+import org.gradle.api.plugins.JavaPluginConvention
 import org.gradle.api.publish.PublishingExtension
 import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.api.tasks.SourceSetContainer
+import org.gradle.api.tasks.bundling.Jar
 import org.gradle.api.tasks.compile.JavaCompile
 import org.gradle.api.tasks.testing.Test
 import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.artifacts
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.create
+import org.gradle.kotlin.dsl.delegateClosureOf
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.get
+import org.gradle.kotlin.dsl.getPlugin
+import org.gradle.kotlin.dsl.named
+import org.gradle.kotlin.dsl.register
 import org.gradle.kotlin.dsl.repositories
+import org.gradle.kotlin.dsl.the
 import org.gradle.kotlin.dsl.withType
+import org.jfrog.gradle.plugin.artifactory.ArtifactoryPlugin
+import org.jfrog.gradle.plugin.artifactory.dsl.ArtifactoryPluginConvention
+import org.jfrog.gradle.plugin.artifactory.dsl.DoubleDelegateWrapper
+import org.jfrog.gradle.plugin.artifactory.dsl.PublisherConfig
+import org.jfrog.gradle.plugin.artifactory.dsl.ResolverConfig
+import org.jfrog.gradle.plugin.artifactory.task.ArtifactoryTask
 
 fun Project.applyCommonConfig(
         group: String = rootProject.group.toString()
 ) {
     apply(plugin = "java-library")
+    apply(plugin = "java")
     apply(plugin = "net.minecrell.licenser")
     apply(plugin = "net.ltgt.apt-idea")
     apply(plugin = "maven-publish")
+    apply(plugin = "com.jfrog.artifactory")
 
     project.group = group
 
@@ -51,16 +68,78 @@ fun Project.applyCommonConfig(
         "testImplementation"(Libs.junitEngine)
     }
 
+    addExtraArchiveArtifacts()
+
+    configureMavenPublish()
+}
+
+private fun Project.addExtraArchiveArtifacts() {
+    tasks.register<Jar>("sourcesJar") {
+        dependsOn("classes")
+        archiveClassifier.set("sources")
+        from(project.the<SourceSetContainer>().getByName("main").allSource)
+    }
+    tasks.register<Jar>("javadocJar") {
+        dependsOn("javadoc")
+        archiveClassifier.set("javadoc")
+        from(tasks.getByName("javadoc"))
+    }
+}
+
+private fun Project.configureMavenPublish() {
     configure<PublishingExtension> {
         publications {
-            create<MavenPublication>("maven") {
+            register<MavenPublication>("maven") {
                 groupId = project.group.toString()
                 artifactId = project.name
                 version = project.version.toString()
 
+                artifact(tasks.getByName("sourcesJar"))
+                artifact(tasks.getByName("javadocJar"))
                 from(components["java"])
             }
         }
+    }
+}
+
+fun Project.configureArtifactory() {
+    apply(plugin = "maven-publish")
+    apply(plugin = "com.jfrog.artifactory")
+    val ext = extensions.extraProperties
+    if (!project.hasProperty("artifactory_contextUrl"))
+        ext["artifactory_contextUrl"] = "http://localhost"
+    if (!project.hasProperty("artifactory_user"))
+        ext["artifactory_user"] = "guest"
+    if (!project.hasProperty("artifactory_password"))
+        ext["artifactory_password"] = ""
+    configure<ArtifactoryPluginConvention> {
+        publish(delegateClosureOf<PublisherConfig> {
+            setContextUrl(project.property("artifactory_contextUrl"))
+            setPublishIvy(false)
+            setPublishPom(true)
+            repository(delegateClosureOf<DoubleDelegateWrapper> {
+                invokeMethod("setRepoKey", when {
+                    "SNAPSHOT" in project.version.toString() -> "libs-snapshot-local"
+                    else -> "libs-release-local"
+                })
+                invokeMethod("setUsername", project.property("artifactory_user"))
+                invokeMethod("setPassword", project.property("artifactory_password"))
+            })
+            defaults(delegateClosureOf<ArtifactoryTask> {
+                publications("maven")
+                setPublishArtifacts(true)
+            })
+        })
+        resolve(delegateClosureOf<ResolverConfig> {
+            repository(delegateClosureOf<DoubleDelegateWrapper> {
+                invokeMethod("setRepoKey", "repo")
+                invokeMethod("setUsername", project.property("artifactory_user"))
+                invokeMethod("setPassword", project.property("artifactory_password"))
+            })
+        })
+    }
+    tasks.named<ArtifactoryTask>("artifactoryPublish") {
+        skip = true
     }
 }
 
