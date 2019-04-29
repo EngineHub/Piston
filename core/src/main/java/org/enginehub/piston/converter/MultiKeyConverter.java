7b578ca5d9caf3b5244c783222ab524bfc0ddb91
Kenzie Togami <ket1999@gmail.com>
2019-04-29 10:26:55 -0700

    Licenses

--- core/src/main/java/org/enginehub/piston/converter/MultiKeyConverter.java
+++ core/src/main/java/org/enginehub/piston/converter/MultiKeyConverter.java
@@ -1,7 +1,7 @@
 /*
- * WorldEdit, a Minecraft world manipulation toolkit
- * Copyright (C) sk89q <http://www.sk89q.com>
- * Copyright (C) WorldEdit team and contributors
+ * Piston, a flexible command management system.
+ * Copyright (C) EngineHub <http://www.enginehub.com>
+ * Copyright (C) Piston contributors
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
